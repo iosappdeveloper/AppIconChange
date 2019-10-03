4e999eeb2c2fdd7a08dd60ba94194982a370ca23
Ashok Matoria <ashok.matoria@sap.com>
2019-10-02 20:26:53 -0400

    Initial Commit

---
+++ AppIconChange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppIconChange
+//
+//  Created by Matoria, Ashok on 02/10/19.
+//  Copyright © 2019 Matoria, Ashok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
